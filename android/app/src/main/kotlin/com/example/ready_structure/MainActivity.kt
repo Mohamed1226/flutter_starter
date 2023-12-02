00b36265be3dca5a3feb9ed40feb9a253b29f3c4
MohamedTawfiq <mohamedun9@gmail.com>
2023-12-02 22:41:54 +0200

    init app

---
+++ android/app/src/main/kotlin/com/example/ready_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ready_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:ready_structure/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
