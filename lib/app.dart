c967e31be0ac5596f0fda1633c29438cf0473d74
MohamedTawfiq <mohamedun9@gmail.com>
2023-12-03 13:44:35 +0200

    add material app settings

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ready_structure/core/di/locator.dart';
+import 'package:ready_structure/core/routing/routes.dart';
+import 'package:ready_structure/core/services/navigation.dart';
+
+import 'core/constant/dimens.dart';
+import 'core/routing/app_router.dart';
+import 'onboarding_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(Dimens.designWidth, Dimens.designHeight),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return MaterialApp(
+            initialRoute: Routes.onBoarding,
+            onGenerateRoute: AppRouter.generateRoute,
+            navigatorKey: locator<NavigationService>().navigatorKey,
+            home: const OnBoardingScreen(),
+          );
+        });
   }
 }
-
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ready_structure/app.dart';
 
 import 'package:ready_structure/main.dart';
 
